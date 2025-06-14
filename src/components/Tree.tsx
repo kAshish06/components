a6821532205ce0767eaa1f169c9fc8f9d1e46e15
kAshish06 <ashish.kumar.0611@gmail.com>
2025-06-14 18:19:45 +0530

    Fixed logical issue with Tree component

--- src/components/Tree.tsx
+++ src/components/Tree.tsx
@@ -19,79 +19,73 @@ type Props = {
 
 const flattenData = (
   list: Data[],
+  selectedIds: number[],
   depth = 0,
   parentId: number | null | undefined = null
-): Omit<FlatData, "checked">[] => {
+): { result: FlatData[]; childMap: Map<number, number[]> } => {
   const result = [];
-  for (let i = 0; i < list.length; i++) {
-    const listNode = list[i];
-    const node: Omit<FlatData, "checked"> = {
+  let nodeChildMap = new Map<number, number[]>();
+  for (const listNode of list) {
+    let checkedStatus: boolean | "indeterminate" = selectedIds.includes(
+      listNode.id
+    );
+    let flatChildData: FlatData[] = [];
+    if (listNode.children?.length) {
+      const { result: childResult, childMap } = flattenData(
+        listNode.children,
+        selectedIds,
+        depth + 1,
+        listNode.id
+      );
+      nodeChildMap.set(listNode.id, [...childResult.map((item) => item.id)]);
+      nodeChildMap = new Map([...nodeChildMap, ...childMap]);
+      const checked = childResult.filter((child) => child.checked)?.length;
+      checkedStatus =
+        checked === childResult.length
+          ? true
+          : checked === 0
+          ? false
+          : "indeterminate";
+      flatChildData = childResult;
+    }
+    const node: FlatData = {
       id: listNode.id,
       name: listNode.name,
       depth: depth,
       parentId: parentId,
+      checked: checkedStatus,
     };
-    result.push(node);
-    if (listNode.children?.length) {
-      result.push(...flattenData(listNode.children, depth + 1, listNode.id));
-    }
+    result.push(node, ...flatChildData);
   }
-  return result;
+  return { result, childMap: nodeChildMap };
 };
-const transformData = (list: Data[], selectedIds: number[]): FlatData[] => {
-  return flattenData(list).map((item) => {
-    return {
-      ...item,
-      checked: selectedIds.includes(item.id),
-    };
-  });
-};
-const findChildren = (list: FlatData[], node: FlatData): number[] => {
-  const children = [];
-  const queue = [node.id];
-  while (queue.length) {
-    const current = queue.shift();
-    const desc = list
-      .filter((item) => item.parentId === current)
-      .map((item) => item.id);
-    children.push(...desc);
-    queue.push(...desc);
-  }
-  return children;
-};
-const getAncestors = (
-  list: FlatData[],
-  node: FlatData | undefined
-): number[] | [] => {
-  const ancestors = [];
-  while (node) {
-    if (node.parentId) {
-      ancestors.push(node.parentId);
-    }
-    node = list.find((item) => item.id === node?.parentId);
-  }
-  return ancestors;
-};
-
-// const getParentsStatus = () => {};
 
 export default function Tree({ data }: Props) {
   const [selectedIds, setSelectedIds] = React.useState<number[]>([]);
 
-  const transformedData = React.useMemo<FlatData[]>((): FlatData[] => {
-    return transformData(data, selectedIds);
+  const { result: transformedData, childMap } = React.useMemo(() => {
+    return flattenData(data, selectedIds);
   }, [selectedIds, data]);
 
   const handleChange = (node: FlatData) => {
-    const children = findChildren(transformedData, node);
+    const children = childMap.get(node.id) || [];
+    let newSelectedIds: number[] = [];
     if (!node.checked) {
-      const newSelectedIds = new Set([...selectedIds, node.id, ...children]);
-      setSelectedIds([...newSelectedIds]);
+      const ids = new Set([...selectedIds, node.id, ...children]);
+      newSelectedIds = [...ids];
     } else {
-      const newSelectedIds = selectedIds.filter(
+      newSelectedIds = selectedIds.filter(
         (id) => ![...children, node.id].includes(id)
       );
-      setSelectedIds(newSelectedIds);
+    }
+    setSelectedIds(newSelectedIds);
+  };
+  const indeterminateHandler = (
+    el: HTMLInputElement | null,
+    item: FlatData
+  ) => {
+    if (el) {
+      el.indeterminate = item.checked === "indeterminate";
     }
   };
   if (!data || data.length === 0) {
@@ -101,15 +95,17 @@ export default function Tree({ data }: Props) {
     <div className="checkboxtree-container">
       {transformedData.map((item) => {
         return (
-          <div style={{ marginLeft: `${10 * item.depth}px` }} key={item.id}>
+          <div style={{ marginLeft: `${20 * item.depth}px` }} key={item.id}>
             <input
+              id={`tree-checkbox-${item.id}`}
               type="checkbox"
               checked={typeof item.checked === "boolean" ? item.checked : false}
               onChange={() => {
                 handleChange(item);
               }}
+              ref={(el) => indeterminateHandler(el, item)}
             />
-            <span>{item.name}</span>
+            <label htmlFor={`tree-checkbox-${item.id}`}>{item.name}</label>
           </div>
         );
       })}
